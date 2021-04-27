89a7be52abd7baf9fb14ed30efc1e83fd89d7b6b
monkyyy <crazymonkyyy@gmail.com>
2021-04-27 11:57:40 -0600

    responding to tyrants demands

--- README.md
+++ README.md
@@ -9,3 +9,22 @@ reload!("file");
 ```
 where "file" refers to the .mix and .conf file name(and so you can have several concurrent settings file
 see example.d and example.mix for syntax
+
+# faq
+
+#### but how do I use this?
+
+mixin setup in a golbal/main/a big scope after the main data's, put the reload it defines in in a keypress event or if statement in the main loop; create a .mix file.
+
+#### Whats a mix file
+
+https://monkyyy.science/posts/copy_and_paste_imports_in_d/ pay attension to the -J bit
+
+#### So how do I access the object produced
+
+You dont, this isn't a json lib; it will reach out and set the values without you, in flippant disregard for encapsulation.
+
+#### But BUt BUT, this is serialization whats the data format?
+
+a endline seperated list of {token}={value};
+Where token can be mixed in and value can be passed to std.conv.to ; I dont know what wont work(if anything) I was surprized when I tested arrays and it did work. Mess with setting.d's readline_ template if you want something to be handled different
--- example.d
+++ example.d
@@ -1,17 +1,17 @@
-void main(){
-	int a,b,c;
-	int[5] array;
-	float f;
-	string mystring;
-	import setting;
-	import std;
+void main(){                           //example.mix
+	int a,b,c;                          //a=1;               
+	int[5] array;                       //b=2;              
+	float f;                            //c=3;              
+	string mystring;                    //mystring="imcute";
+	import setting;                     //array=[1,2,3,4,5];
+	import std;                         //f=3.14;           
 	void error(){
 		"config didnt work, made one for you at example.conf".writeln;
 	}
 	mixin setup!("example",error);
-	reload!"example";
-	writeln("a: ",a," b: ", b," c: ",c);
-	writeln("array: ",array);
-	writeln("floats: ",f);
-	writeln("strings: ", mystring);
+	reload!"example";                    //dmd -J=. -i -run example.d
+	writeln("a: ",a," b: ", b," c: ",c); //a: 1 b: 2 c: 3            
+	writeln("array: ",array);            //array: [1, 2, 3, 4, 5]    
+	writeln("floats: ",f);               //floats: 3.14              
+	writeln("strings: ", mystring);      //strings: imcute           
 }
